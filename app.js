6091ae12b58faf647ef8ce1438f5e4bc5761b29d
Jonathan <jrvasquez54@gmail.com>
2020-10-12 10:22:05 -0500

    second save

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const path = require('path')
 const express = require('express')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 const connectDB = require('./config/db')
+const { use } = require('passport')
 
 //Load config
 dotenv.config({path: './config/config.env'})
 
+//Passport config
+require('./config/passport')(passport)
+
+
 connectDB()
 
 const app = express()
+
+// Body Parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 //Logging
 if(process.env.NODE_ENV === 'development'){
@@ -21,11 +34,27 @@ if(process.env.NODE_ENV === 'development'){
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
+
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+}))
+
+
+//Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 3000
 
